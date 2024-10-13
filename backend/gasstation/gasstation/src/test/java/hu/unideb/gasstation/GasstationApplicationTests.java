2a7054f9614e7e394360c45e23aa9fd3e4fae737
attila1020 <attila.miszkuly@gmail.com>
2024-10-13 16:07:39 +0000

    Backend init commit

---
+++ backend/gasstation/gasstation/src/test/java/hu/unideb/gasstation/GasstationApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.unideb.gasstation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GasstationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
