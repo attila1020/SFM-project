09c6aeb8f9cf3d3c284cbe7e184c4cf48a9c47e0
attila1020 <attila.miszkuly@gmail.com>
2024-11-07 18:48:28 +0000

    Created two endpoints

--- backend/gasstation/gasstation/src/main/java/hu/unideb/gasstation/GasstationApplication.java
+++ backend/gasstation/gasstation/src/main/java/hu/unideb/gasstation/GasstationApplication.java
@@ -2,8 +2,9 @@ package hu.unideb.gasstation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class GasstationApplication {
 
 	public static void main(String[] args) {
