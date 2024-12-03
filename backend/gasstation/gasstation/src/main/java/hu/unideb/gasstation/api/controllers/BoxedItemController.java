40f5eaa46225dacc95b786d3b08380eafc1daa63
attila1020 <95174888+attila1020@users.noreply.github.com>
2024-12-03 20:37:00 +0100

    Added BoxedItemController

---
+++ backend/gasstation/gasstation/src/main/java/hu/unideb/gasstation/api/controllers/BoxedItemController.java
@@ -0,0 +1,35 @@
+package hu.unideb.gasstation.api.controllers;
+
+import hu.unideb.gasstation.models.BoxedItems;
+import hu.unideb.gasstation.repositories.BoxedItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class BoxedItemController {
+
+    @Autowired
+    BoxedItemRepository boxedItemRepository;
+
+    @GetMapping("/boxed")
+    public List<BoxedItems> getBoxedItems() {
+        return boxedItemRepository.findAll();
+    }
+
+    @PostMapping("/boxed/{id}")
+    public boolean updateBoxedItem(@PathVariable Long id, @RequestBody BoxedItems boxedItem) {
+        if (boxedItemRepository.existsById(id)) {
+            boxedItemRepository.save(boxedItem);
+            return true;
+        }
+        return false;
+    }
+
+    @PostMapping("/boxed")
+    public BoxedItems addBoxedItem(@RequestBody BoxedItems boxedItem) {
+        return boxedItemRepository.save(boxedItem);
+    }
+}
